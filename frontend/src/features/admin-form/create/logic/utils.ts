1e702d26e4ee1fea8429cf48100f03a2aae40f73
Kar Rui Lau <karrui.lau@gmail.com>
2022-03-08 15:19:52 +0800

    feat: update EditCondition form return shape to be FormLogicBase shape

--- frontend/src/features/admin-form/create/logic/utils.ts
+++ frontend/src/features/admin-form/create/logic/utils.ts
@@ -1,7 +1,39 @@
-import { LogicDto, LogicType, ShowFieldLogicDto } from '~shared/types/form'
+import { BasicField } from '~shared/types/field'
+import {
+  LogicConditionState,
+  LogicDto,
+  LogicIfValue,
+  LogicType,
+  ShowFieldLogicDto,
+} from '~shared/types/form'
 
 export const isShowFieldsLogic = (
   formLogic: LogicDto,
 ): formLogic is ShowFieldLogicDto => {
   return formLogic.logicType === LogicType.ShowFields
+}
+
+export const getIfLogicType = ({
+  fieldType,
+  conditionState,
+}: {
+  fieldType: BasicField
+  conditionState: LogicConditionState
+}) => {
+  // Default logic block type
+  if (!fieldType) return LogicIfValue.SingleSelect
+
+  switch (fieldType) {
+    case BasicField.Dropdown:
+    case BasicField.Radio: {
+      return conditionState === LogicConditionState.Equal
+        ? LogicIfValue.SingleSelect
+        : LogicIfValue.MultiSelect
+    }
+    case BasicField.Rating:
+    case BasicField.YesNo:
+      return LogicIfValue.SingleSelect
+    default:
+      return LogicIfValue.Number
+  }
 }
